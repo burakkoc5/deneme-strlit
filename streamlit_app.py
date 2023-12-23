ded832b7385636aee2368ae7439ffa654cd37549
Burak Koç <74610612+burakkoc5@users.noreply.github.com>
2023-12-23 13:46:21 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,24 @@
 import streamlit as st
 
+
+# Streamlit uygulaması
+st.title("Veri Bilimi Modeli Dashboard")
+
+# Veri setini gösterme
+st.subheader("Veri Seti")
+# Model Performansı
+st.subheader("Model Performansı")
+# Eğitim seti doğruluğu
+st.write(f"Eğitim Seti Doğruluğu: ")
+
+
+# Kullanıcıdan özellik değerlerini girmesini isteme
+sepal_length = st.slider("Sepal Length", float(X['sepal length (cm)'].min()), float(X['sepal length (cm)'].max()), float(X['sepal length (cm)'].mean()))
+sepal_width = st.slider("Sepal Width", float(X['sepal width (cm)'].min()), float(X['sepal width (cm)'].max()), float(X['sepal width (cm)'].mean()))
+petal_length = st.slider("Petal Length", float(X['petal length (cm)'].min()), float(X['petal length (cm)'].max()), float(X['petal length (cm)'].mean()))
+petal_width = st.slider("Petal Width", float(X['petal width (cm)'].min()), float(X['petal width (cm)'].max()), float(X['petal width (cm)'].mean()))
+st.write(f"Test Seti Doğruluğu: }")
+
+# Sınıflandırma tahmini
+st.subheader("Sınıflandırma Tahmini")
 st.write('Hello world!')
