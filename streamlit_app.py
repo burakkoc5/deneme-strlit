694c615f9ac02fb59fdb4a01f0f20900f83307f9
Burak Koç <74610612+burakkoc5@users.noreply.github.com>
2023-12-23 13:47:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,10 +13,8 @@ st.write(f"Eğitim Seti Doğruluğu: ")
 
 
 # Kullanıcıdan özellik değerlerini girmesini isteme
-sepal_length = st.slider("Sepal Length", float(X['sepal length (cm)'].min()), float(X['sepal length (cm)'].max()), float(X['sepal length (cm)'].mean()))
-sepal_width = st.slider("Sepal Width", float(X['sepal width (cm)'].min()), float(X['sepal width (cm)'].max()), float(X['sepal width (cm)'].mean()))
-petal_length = st.slider("Petal Length", float(X['petal length (cm)'].min()), float(X['petal length (cm)'].max()), float(X['petal length (cm)'].mean()))
-petal_width = st.slider("Petal Width", float(X['petal width (cm)'].min()), float(X['petal width (cm)'].max()), float(X['petal width (cm)'].mean()))
+sepal_length = st.slider("Sepal Length")
+
 st.write(f"Test Seti Doğruluğu:")
 
 # Sınıflandırma tahmini
